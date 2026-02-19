a8d8bb72834db677e6a6386e245d68927f4c4bff
itgostack-ux <it.gostack@gmail.com>
2026-02-19 18:55:24 +0530

    Initial FastAPI upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI server is running 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
